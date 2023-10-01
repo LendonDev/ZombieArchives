29e57f8ad9423ba3d87cbefa3df904a064fc0162
LendonDev <88983553+LendonDev@users.noreply.github.com>
2023-10-01 02:16:12 +0100

    Update Navbar.jsx

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import MenuIcon from '@mui/icons-material/Menu';
+import LogoImg from '../temp-logo.png';
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import PersonIcon from '@mui/icons-material/Person';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import EmailIcon from '@mui/icons-material/Email';
+
+
 
 function Navbar() {
+
+  function handleClick (props) {
+    const Btn1 = document.querySelector(".navbar_button"); {
+
+      alert("Great Shot!");
+      console.log("navbar button clicked.");
+    }
+  }
+  
   return (
       <nav className="navbar-main">
         <ul className="navbar-main__list">
           <li className="navbar-main__list-item list-item__menu-button">
-          <button className="navbar__button">
+          <button onClick={handleClick}className="navbar__button">
           <MenuIcon/>
         </button>
           </li>
-          <li className="navbar-main__list-item list-item__menu-logo">
-          <img src="#.jpg"></img>
-          </li>
+          <a className="navbar-main__link"href="/#">
+            <li className="navbar-main__list-item list-item__menu-logo">
+              <img src={LogoImg} className="App-logo" alt="logo" style={{width:140}}/>
+            </li>
+          </a>
           <li className="navbar-main__list-item list-item__menu-item">
             <a className="navbar-main__link"href="/#">
               <div className="list-item__menu-item-inner">About</div>
@@ -37,15 +55,43 @@ function Navbar() {
           </li>
           <li className="navbar-main__list-item list-item__menu-item">
             <a className="navbar-main__link"href="/#">
-              <div className="list-item__menu-item-inner">Submit Game</div>
+              <div className="list-item__menu-item-inner">Submit Game<ArrowOutwardIcon/></div>
             </a>
           </li>
-          </ul>
-          <div className="navbar-main__list-item list-item__menu-button">
+          <div className="navbar-main__list-item list-item__manage-button">
             <button className="navbar__button"style={{width:100}}>
               <ArrowDropDownIcon/>
               <PersonIcon/>
             </button>
+          </div>
+          </ul>
+          <div className="navbar-links">
+            <div className="navbar-links__item">
+              <p>Connect with us:</p>
+            </div>
+            <a className='navbar-main__link'href="/#">
+              <div className="navbar-links__item">
+                <YouTubeIcon/>
+              </div>
+            </a>
+            <a className='navbar-main__link'href="/#">
+              <div className="navbar-links__item">
+                <TwitterIcon/>
+              </div>
+            </a>
+            <a className='navbar-main__link'href="/#">
+              <div className="navbar-links__item">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-discord" viewBox="0 0 16 16">
+                <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z"/>
+            </svg>
+              </div>
+            </a>
+            <a className='navbar-main__link'href="/#">
+              <div className="navbar-links__item">
+                <EmailIcon/>
+              </div>
+            </a>
+
           </div>
       </nav>
   );
