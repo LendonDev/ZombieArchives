442a0badb2b1d43ac2a919bb9c4fc7ce8bc9639b
LendonDev <88983553+LendonDev@users.noreply.github.com>
2023-10-03 22:02:01 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import NavbarAlert from './components/NavbarAlert';
 import Navbar from './components/Navbar';
-import SideNav from './components/SideNav';
 import HeroSmall from './components/HeroSmall';
 
 function App() {
   return (
     <div>
-      <header className="header">
-      <NavbarAlert/>
-      <Navbar/>
-      <SideNav/>
-      </header>
-      <HeroSmall/>
+
+      <body>
+        <header className="header">
+          <Navbar/> 
+        </header>
+          <HeroSmall/>
+
+      </body>
     </div>
   );
 }
